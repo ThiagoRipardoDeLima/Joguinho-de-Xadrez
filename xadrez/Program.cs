4fa5e855d3c2144917e8a36d0b7ab164189f5554
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2018-05-28 16:02:17 -0400

    adicionado arquivos readme e license e criado a classe posicao

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez
 {
@@ -6,7 +7,14 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3,4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+
         }
     }
 }
