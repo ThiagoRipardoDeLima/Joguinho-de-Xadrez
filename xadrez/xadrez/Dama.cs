8c99943a6d0e3dd01ba995ac546398b67da8b265
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2018-06-01 23:59:08 -0400

    Acrescentando as demais pecas

---
+++ xadrez/xadrez/Dama.cs
@@ -0,0 +1,117 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Dama : Peca
+    {
+        public Dama(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor) { }
+
+        public override string ToString()
+        {
+            return "D";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != this.cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //esquerda
+            pos.definirPosicao(posicao.linha, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha, pos.coluna - 1);
+            }
+
+            //direita
+            pos.definirPosicao(posicao.linha, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha, pos.coluna + 1);
+            }
+
+            //Acima
+            pos.definirPosicao(posicao.linha - 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha - 1, pos.coluna);
+            }
+
+            //Abaixo
+            pos.definirPosicao(posicao.linha + 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha + 1, pos.coluna);
+            }
+
+            //NO - Noroeste
+            pos.definirPosicao(posicao.linha - 1, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha - 1, pos.coluna - 1);
+            }
+
+            //NE - Nordeste
+            pos.definirPosicao(posicao.linha - 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha - 1, pos.coluna + 1);
+            }
+
+            //SE - Sudeste
+            pos.definirPosicao(posicao.linha + 1, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha + 1, pos.coluna + 1);
+            }
+
+            //SO - Sudoeste
+            pos.definirPosicao(posicao.linha + 1, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                    break;
+
+                pos.definirPosicao(pos.linha + 1, pos.coluna - 1);
+            }
+
+            return mat;
+        }
+    }
+}
